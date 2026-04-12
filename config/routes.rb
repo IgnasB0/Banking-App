65d78d06528f58fcacc546d9ce68a516c55b1565
Ignas Bujevičius <49751122+IgnasB0@users.noreply.github.com>
2026-04-12 13:24:24 +0300

    routes fix

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,11 @@
 Rails.application.routes.draw do
-  resources :deposits, only: [ :create ]
+  resource :session
+resources :deposits, only: [ :create ]
   resources :withdrawals, only: [ :create ]
-  resources :transactions, only: [ :create ]
+  resources :transactions, only: [ :new, :create ]
   resources :accounts, only: [ :index, :create, :show ]
+
+  root "accounts#index"
 
   get "up" => "rails/health#show", as: :rails_health_check
 end
