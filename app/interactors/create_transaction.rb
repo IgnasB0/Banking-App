56274d575c8d004d97f9623e5c78b7d9e7542c64
Ignas Bujevičius <49751122+IgnasB0@users.noreply.github.com>
2026-04-12 13:23:42 +0300

    Transfers using iban
    Transfers will now use the IBAN number instead of account id.

--- app/interactors/create_transaction.rb
+++ app/interactors/create_transaction.rb
@@ -1,27 +1,41 @@
+# frozen_string_literal: true
+
+# Transfers an amount from one account to another, looked up by IBAN.
 class CreateTransaction
   include Interactor
 
   def call
-    from_account = Account.find_by(id: context.transaction_params[:from_account_id])
+    from_account = find_sender
+    to_account = find_recipient
+    verify_balance(from_account)
+    save_transaction(from_account, to_account)
+  end
 
-    unless from_account
-      context.fail!(errors: 'Sender account not found')
-      return
-    end
+  private
 
-    to_account = Account.find_by(id: context.transaction_params[:to_account_id])
-    unless to_account
-      context.fail!(errors: 'Recipient account not found')
-      return
-    end
+  def find_sender
+    account = Account.find_by(id: context.from_account_id)
+    context.fail!(errors: 'Sender account not found') unless account
+    account
+  end
 
+  def find_recipient
+    account = Account.find_by(iban: context.to_iban)
+    context.fail!(errors: 'Recipient account not found') unless account
+    account
+  end
+
+  def verify_balance(from_account)
     balance = CalculateAccountBalance.call(account: from_account).balance
-    if balance < context.transaction_params[:amount].to_d
-      context.fail!(errors: 'Insufficient balance')
-      return
-    end
+    context.fail!(errors: 'Insufficient balance') if balance < context.amount.to_d
+  end
 
-    transaction = Transaction.new(context.transaction_params)
+  def save_transaction(from_account, to_account)
+    transaction = Transaction.new(
+      from_account_id: from_account.id,
+      to_account_id: to_account.id,
+      amount: context.amount
+    )
     if transaction.save
       context.transaction = transaction
     else
